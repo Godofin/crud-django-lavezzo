0999c369a61f46b2108ee688709a9dca7a8c3ef6
Guilherme Lavezzo <guilherm909088@gmail.com>
2022-10-30 21:45:12 -0300

    Refactoring form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from app.models import Games
 
 class GamesForm(ModelForm):
     class Meta:
         model = Games
         fields = ['title', 'category', 'console', 'asin', 'language']
+        widgets = {
+            'title': TextInput(attrs={'class':'form-control'}),
+            'category': TextInput(attrs={'class':'form-control'}),
+            'console': TextInput(attrs={'class':'form-control'}),
+            'asin': TextInput(attrs={'class':'form-control'}),
+            'language': TextInput(attrs={'class':'form-control'}),
+        }
--- app/views.py
+++ app/views.py
@@ -22,7 +22,6 @@ def view(request, pk):
     data = {}
     data['game'] = Games.objects.get(pk=pk)
     return render(request, 'view.html', data)
-
 def edit(request, pk):
     data = {}
     data['game'] = Games.objects.get(pk=pk)
@@ -41,3 +40,4 @@ def delete(request, pk):
     db = Games.objects.get(pk=pk)
     db.delete()
     return redirect('home')
+
